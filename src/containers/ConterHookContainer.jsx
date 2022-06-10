e0939f8ea89b46e98fb10da5c494b6aae035fb82
hjseong <hjseong1222@gmail.com>
2022-06-10 10:27:40 +0900

    hook 을 이용한 컨테이너 최적화

--- src/containers/ConterHookContainer.jsx
+++ src/containers/ConterHookContainer.jsx
@@ -1,6 +1,7 @@
-// 리덕스는 Hook 이전에 생긴 라이브러리.
+// 리덕스는 Hook 이전에 생긴 라이브러리 - 함수형에서만 사용가능
 // 리액트 16.8버전에서 Hook이 도입되면서 Hook을 이용한 내용이 추가
 // 현 리액트 18.0 버전
+import { useCallback } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import Counter from "../components/Counter";
 import { increase, decrease } from "../modules/counter";
@@ -9,8 +10,12 @@ const CounterHookContainer = () => {
   // state에 접근하기위해 useSelector 사용
   const number = useSelector((state) => state.counter.number);
   // dispatch를 사용하기 위해 useDispatch 사용
-
   const dispatch = useDispatch();
-  return <Counter number={number} onIncrease={() => dispatch(increase())} onDecrease={() => dispatch(decrease())} />;
+
+  // Callback 함수를 이용해서 최적화 : 함수 새로 만듦 방지
+  const onIncrease = useCallback(() => dispatch(increase()), [dispatch]);
+  const onDecrease = useCallback(() => dispatch(decrease()), [dispatch]);
+
+  return <Counter number={number} onIncrease={onIncrease} onDecrease={onDecrease} />;
 };
 export default CounterHookContainer;
